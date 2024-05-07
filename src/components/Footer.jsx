af0a103f69dc2cc62bc2b2bebcd8c3e6550b484d
Devendra <devendrakhatri904@gmail.com>
2024-05-07 13:33:16 +0545

    working with nasa api

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ullam
+        odit delectus ipsa quisquam magni iste qui beatae explicabo pariatur!
+        Animi veritatis excepturi molestiae eveniet ab, quisquam quae doloribus
+        vel!
+      </p>
+      <h1>Nasa Api</h1>
+      <button>
+        <i className="ri-expand-left-line"></i>
+      </button>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="h-[80vh] w-screen bg-zinc-500   overflow-hidden bg-center">
+      <img
+        className="h-full w-full"
+        src="https://images.unsplash.com/photo-1614728263952-84ea256f9679?q=80&w=1908&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Main;
