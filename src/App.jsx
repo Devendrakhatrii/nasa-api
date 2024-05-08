555ee7306fc582ce6fea8ab4a1e6aa8491d4a495
Devendra khatri <devendrakhatri904@gmail.com>
2024-05-08 15:57:14 +0545

    handled toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import SideBar from "./components/SideBar";
 
 function App() {
+  const [description, setDescription] = useState(false);
+  const handleToggle = () => {
+    setDescription(!description);
+  };
   return (
     <>
       <Main />
-      <SideBar />
-      <Footer />
+      {description && <SideBar handleToggle={handleToggle} />}
+      <Footer handleToggle={handleToggle} />
     </>
   );
 }
